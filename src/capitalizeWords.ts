a2a1b7f9b180652afe7cd4922463a4d3c0bfa6c9
Lucy-Jane Walsh <ljewalsh@gmail.com>
2021-10-20 19:11:00 +0200

    feat(lib): add capitalizeWords

--- src/capitalizeWords.ts
+++ src/capitalizeWords.ts
@@ -4,5 +4,17 @@
 
 export const capitalizeWords = (text: string): string => {
   //
-  return text
+  // Split the text on space to get individual words
+  // Loop through the words in the list
+  // For each word, capitalise the first letter and add to the string to return
+  const words = text.split(' ')
+  return words.reduce((acc, word) => {
+    const firstLetterCapitalised = word[0].toUpperCase()
+    if (acc === '') {
+      acc += firstLetterCapitalised + word.slice(1)
+    } else {
+      acc += ' ' + firstLetterCapitalised + word.slice(1)
+    }
+    return acc
+  }, '')
 }
